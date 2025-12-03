0f2e86ad1e18ffc9fae538987c31dcb7eedc0314
José Angel Alicea Santiago <jas487432@gmail.com>
2025-12-02 23:56:33 -0500

    Fix parameters in get_max_health function call
    Replaced parameters in get_max_health call to use my_modifier and my_level.

--- ScopeProblem1.py
+++ ScopeProblem1.py
@@ -7,7 +7,7 @@ my_level = 10
 
 ## don't touch above this line
 
-max_health = get_max_health(my_modifier, my_level)
+max_health = get_max_health(my_modifier, my_level) #solved, solution: replaced parameter with my_modifier and my_level instead of modifier and level
 
 # don't touch below this line
 
